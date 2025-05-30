cfb5c9ac579a76da7a4836a0b75b6496f7e5c40c
akashsharma230197 <pinkisharma9811067274@gmail.com>
2025-05-30 19:49:23 +0530

    Database added

--- routes/purchase.js
+++ routes/purchase.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const pool = require('../db'); // your PostgreSQL connection
+const pool = require('../db/database'); // your PostgreSQL connection
 
 // GET all units
 router.get('/Units', async (req, res) => {
